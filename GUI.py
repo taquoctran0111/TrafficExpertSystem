4867e2b6cd5bc80cb34d27f60ba6ecc159a8fac4
taquoctran <taquoctrantb2000@gmail.com>
2021-10-21 09:32:59 +0700

    update main and GUI

--- GUI.py
+++ GUI.py
@@ -1,65 +1,83 @@
 import PySimpleGUI as sg
+import main as expert
 
 frame = [
     [sg.Text("Tốc độ tối đa: ", size=(12,1)), sg.Text(" ",size=(582,1), key="max_output", text_color="black")], 
     [sg.Text("Tốc độ tối thiểu: ", size=(12,1)), sg.Text(" ",size=(582,1), key="min_output", text_color="black")],
-    [sg.Text("Lời khuyên: ", size=(12,1)), sg.Text(" ",size=(582,1), key="advice_output", text_color="black")]
+    [sg.Text("Lời khuyên: ", size=(12,1))],
+    [sg.Text(" ",size=(700,4), key="advice_output", text_color="black")]
 ]
 
 layout = [
     [sg.Text("------ ĐIỀU KIỆN ĐƯỜNG VÀ HIỆN TRẠNG GIAO THÔNG")],
     [sg.Text(" ")],
-    [sg.Checkbox('Có phương tiện trong vòng 100m phía trước', default=False)],
-    [sg.Text("Số làn: ", size=(35,1)), sg.Radio('Một làn', "RADIO1", default=True), sg.Radio('Hai làn', "RADIO1", default=False), sg.Radio('Ba làn', "RADIO1", default=False)],
-    [sg.Text("Phương tiện đang lưu thông trên làn: ", size=(35,1)), sg.Radio('Làn trái', "RADIO2", default=True), sg.Radio('Làn giữa', "RADIO2", default=False), sg.Radio('Làn phải', "RADIO2", default=False)],
-    [sg.Text("Tầm nhìn nhỏ hơn: ", size=(35,1)), sg.Radio('Không bị hạn chế', "RADIO3", default=True), sg.Radio('200m', "RADIO3", default=False), sg.Radio('100m', "RADIO3", default=False), sg.Radio('50m', "RADIO3", default=False)],
-    [sg.Text("Trạng thái đường đi: ", size=(35,1)), sg.Checkbox('Đường trơn trượt', default=False), sg.Checkbox('Địa hình quanh co, đèo dốc', default=False)],
+    [sg.Checkbox('Có phương tiện trong vòng 100m phía trước', default=False, key="safety")],
+    [sg.Text("Số làn: ", size=(35,1)), sg.Radio('Một làn', "RADIO1", default=True, key="1lane"), sg.Radio('Hai làn', "RADIO1", default=False, key="2lane"), sg.Radio('Ba làn', "RADIO1", default=False, key="3lane")],
+    [sg.Text("Phương tiện đang lưu thông trên làn: ", size=(35,1)), sg.Radio('Không chia làn', "RADIO2", default=True, key="lane-n"), sg.Radio('Làn trái', "RADIO2", default=False, key="lane-l"), sg.Radio('Làn giữa', "RADIO2", default=False, key="lane-c"), sg.Radio('Làn phải', "RADIO2", default=False, key="lane-r")],
+    [sg.Text("Tầm nhìn nhỏ hơn: ", size=(35,1)), sg.Radio('Không bị hạn chế', "RADIO3", default=True, key="without"), sg.Radio('200m', "RADIO3", default=False, key="200m"), sg.Radio('100m', "RADIO3", default=False, key="100m"), sg.Radio('50m', "RADIO3", default=False, key="50m")],
+    [sg.Text("Trạng thái đường đi: ", size=(35,1)), sg.Checkbox('Đường trơn trượt', default=False, key="slippery"), sg.Checkbox('Địa hình quanh co, đèo dốc', default=False, key="highpass")],
     [sg.Button("Tư vấn")],
     [sg.Frame("Kết quả tư vấn", frame, key='-FRAME-')]
 ]
 
-window = sg.Window("Tư vấn tốc độ xe", layout, size=(650,350))
+window = sg.Window("Tư vấn tốc độ xe", layout, size=(700,420))
 
 
 while True:
     event, values = window.read()
 
-    facts = ['Freeway']
+    def get_facts():
+        facts = ['Freeway']
 
-    
-
-    if values[0]:
-        facts.append("Safety distance 100 meters")
-    if values[1]:
-        facts.append("One lane")
-    if values[2]:
-        facts.append("Two lane")
-    if values[3]:
-        facts.append("Three lane")
-    if values[4]:
-        facts.append("Left lane")
-    if values[5]:
-        facts.append("Middle lane")
-    if values[6]:
-        facts.append("Right lane")
-    if values[7]:
-        facts.append("Without")
-    if values[8]:
-        facts.append("200m")
-    if values[9]:
-        facts.append("100m")
-    if values[10]:
-        facts.append("50m")
-    if values[11]:
-        facts.append("Slippery")
-    if values[12]:
-        facts.append("High pass")
+        if values["safety"]:
+            facts.append("Safety distance 100 meters")
+        # if values["1lane"]:
+        #     facts.append("One lane")
+        if values["2lane"]:
+            facts.append("Two lanes")
+        if values["3lane"]:
+            facts.append("Three lanes")
+        if values["lane-l"]:
+            facts.append("Left lane")
+        if values["lane-c"]:
+            facts.append("Middle lane")
+        if values["lane-r"]:
+            facts.append("Right lane")
+        # if values["without"]:
+        #     facts.append("Without")
+        if values["200m"]:
+            facts.append("200m")
+        if values["100m"]:
+            facts.append("100m")
+        if values["50m"]:
+            facts.append("50m")
+        if values["slippery"]:
+            facts.append("Slippery")
+        if values["highpass"]:
+            facts.append("High pass")
+        
+        return facts
 
 
     if event == "Tư vấn":
-        print(facts)
-        window['max_output'].update("120 km/h")
-        window['min_output'].update("100 km/h")
-        window['advice_output'].update("Đi chậm lại đi cmm =)))")
+        facts = get_facts()
+        rules = expert.import_rules()
+        Max, Min, Advice = expert.test_one_case(rules, facts)
+        window['max_output'].update(str(Max)+"km/h")
+        window['min_output'].update(str(Min)+"km/h")
+        
+        if len(Advice) > 0:
+            t = ''
+            if 'Open lights' in Advice:
+                t += '- Yêu cầu bật đèn!\n'
+            if 'Leave ASAP' in Advice:
+                t += '- Tầm nhìn quá thấp, rời khỏi đường cao tốc tại lối ra gần nhất!\n'
+            if 'Slippery' in Advice:
+                t += '- Đường trơn trượt, yêu cầu đi chậm lại!\n'
+            if 'High pass' in Advice:
+                t += '- Điều kiện đường xấu, giảm tốc độ phương tiện và đảm bảo khoảng cách an toàn!\n'
+            window['advice_output'].update(t)
+        else:
+            window['advice_output'].update("- Đảm bảo khoảng cách an toàn giữa các phương tiện!")
     if event == sg.WIN_CLOSED:
         break
